9cfcc9fc20f2a17ee8b75bcc8db43f9e0fadd53d
SayedJPQ <sayedjquiros@gmail.com>
2021-04-16 12:16:03 -0600

    Curso C++

---
+++ Tipos de datos/Tipos de datos.cpp
@@ -0,0 +1,22 @@
+//Tipos de datos basicos
+#include<iostream>
+using namespace std;
+int main(){
+// Declarar una variable tipo entero
+	int entero = 10;
+// Declarar una variable flotante
+	float flotante = 12.32;
+// Declarar una variable double ya que puede contener mas numeros
+	double mayor = 12091231;
+// Declarar una variable tipo texto
+	char letra = 'S';
+// IMprimir datos
+	cout<<entero<<endl;
+	cout<<""<<endl;
+	cout<<flotante<<endl;
+	cout<<""<<endl;
+	cout<<mayor<<endl;
+	cout<<""<<endl;
+	cout<<letra<<endl;
+	return 0;
+}
