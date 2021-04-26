5f1d6dd52cf339aa3fb1b63698ce85d6a8412ba1
SayedJPQ <sayedjquiros@gmail.com>
2021-04-26 10:12:28 -0600

    Agregando operaciones aritmeticas

---
+++ Entrada de datos/Entrada de datos.cpp
@@ -0,0 +1,14 @@
+//Entrada de datos
+/* Comentario
+de 
+varias lineas
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	float numero;
+	cout<<"Digite un numero: ";
+	cin>>numero;
+	cout<<"\nEl numero que digito es: "<<numero;
+	return 0;
+}
---
+++ Operaciones arimeticas/Operaciones aritmeticas.cpp
@@ -0,0 +1,17 @@
+//Operaciones aritmeticas basicas con entradas de datos
+#include<iostream>
+using namespace std;
+int main(){
+	float n1,n2, suma = 0, resta = 0, multiplicacion = 0, division = 0;
+	cout<<"Digite un numero: "; cin>>n1;
+	cout<<"Digite otro numero: "; cin>>n2;
+	suma = n1 + n2;
+	resta = n1 - n2;
+	multiplicacion = n1 * n2;
+	division = n1 / n2;
+	cout<<"\nLa suma es: "<<suma<<endl;
+	cout<<"La resta es: "<<resta<<endl;
+	cout<<"La multiplacion es: "<<multiplicacion<<endl;
+	cout<<"La division es: "<<division<<endl;
+	return 0;
+}
