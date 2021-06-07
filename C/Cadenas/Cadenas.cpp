8e13f2d8dfa7749d85ae234bd1d57ac18e7f30c5
SayedJPQ <sayedjquiros@gmail.com>
2021-06-07 10:56:13 -0600

    Cadenas

---
+++ C/Cadenas/Cadenas.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main(){
+	char palabra[] = "Sayed";
+	char nombre[30];
+	cout<<"Digite su nombre: ";cin.getline(nombre,30,'\n');
+	cout<<nombre<<endl;
+	getch();
+	return 0;
+}
