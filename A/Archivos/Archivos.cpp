cc71b395d055b78716efda50feea24e698ce8d94
SayedJPQ18 <79384930+SayedJPQ@users.noreply.github.com>
2022-08-02 19:40:51 -0600

    Creacion y escritura de archivos

---
+++ A/Archivos/Archivos.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+using namespace std;
+void escribir();
+int main(){
+	escribir();
+	system("pause");
+	return 0;
+}
+void escribir(){
+	ofstream archivo;
+	string nombre,frase;
+	cout<<"Digite nombre de archivo: ";
+	getline(cin,nombre);
+	archivo.open(nombre.c_str(),ios::out);
+	if(archivo.fail()){
+		cout<<"Error";
+		exit(1);
+	}
+	cout<<"\nDigite texto del archivo: ";
+	getline(cin,frase);
+	archivo<<frase;
+	archivo.close();
+}
